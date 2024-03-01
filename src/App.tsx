c1039f0644029caef5abbd9079bb9981cd883836
iFeyz <ifeyz17@gmail.com>
2024-03-01 22:26:13 +0100

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chessboard from './components/Chessboard';
+
+function App() {
+  return (
+    <div id="app">
+      <Chessboard/>
+    </div>
+  );
+}
+
+export default App;
